6e476626b9ac39dbf7d9618cc1ca40e3c2afb34a
yangchangzhi <690674930@qq.com>
2018-01-09 17:09:54 +0800

    add presentation layer

---
+++ RepositoryPattern/DDD.Learning/DDD.Learning.Repository.Impl/Repository/CustomerRepository.cs
@@ -0,0 +1,12 @@
+﻿using DDD.Learning.Domain;
+using DDD.Learning.Domain.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDD.Learning.Repository.Memory.Repository
+{
+    public class CustomerRepository
+    {
+    }
+}
