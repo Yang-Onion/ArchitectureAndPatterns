4b07f1619fb1d826c946b6e7d4dad7447e624ada
yangchangzhi <690674930@qq.com>
2018-01-12 10:21:42 +0800

    override equals

--- RepositoryPattern/RepositoryPattern.WebApi/RepositoryPattern.Domain/Entity.cs
+++ RepositoryPattern/RepositoryPattern.WebApi/RepositoryPattern.Domain/Entity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 
 namespace RepositoryPattern.Domain
@@ -7,6 +8,46 @@ namespace RepositoryPattern.Domain
     public abstract class Entity<TPrimaryKey>:IEntity<TPrimaryKey>
     {
         public virtual TPrimaryKey Id { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj==null || !(obj is Entity<TPrimaryKey>))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this,obj))
+            {
+                return true;
+            }
+
+            var other = (Entity<TPrimaryKey>)obj;
+            var typeOfThis = this.GetType();
+            var typeOfOther = other.GetType();
+            if (!typeOfThis.GetTypeInfo().IsAssignableFrom(typeOfOther) && !typeOfOther.GetTypeInfo().IsAssignableFrom(typeOfThis))
+            {
+                return false;
+            }
+            return Id.Equals(other.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public static bool operator ==(Entity<TPrimaryKey> left,Entity<TPrimaryKey> right)
+        {
+            if (Equals(left,null))
+            {
+                return Equals(right, null);
+            }
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Entity<TPrimaryKey> left, Entity<TPrimaryKey> right)
+        {
+            return !(left == right);
+        }
     }
 
     public abstract class Entity : Entity<string>, IEntity
